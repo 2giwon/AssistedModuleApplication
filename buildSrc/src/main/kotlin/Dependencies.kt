6c268163a8d1be68e6d9eee8c5ac902c135bdeb7
middlefgr <2giwon@gmail.com>
2022-07-05 19:32:39 +0900

    [Add] - dagger2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,17 @@
 object Version {
     const val KOTLIN_VER = "1.5.31"
-
+    const val DAGGER2_VER = "2.27"
+    const val ASSISTED_INJECT_VER = "0.5.2"
 }
 
 object ProjectConfig {
     const val GRADLE = "com.android.tools.build:gradle:7.0.3"
+    const val KOTLIN_GRADLE_PLUGIN =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN_VER}"
 }
 
 object AndroidConfig {
-    const val COMPILE_SDK = 30
+    const val COMPILE_SDK = 31
     const val APP_ID = "com.egiwon.assistedmoduleapplication"
     const val MIN_SDK = 23
     const val VERSION_CODE = 1
@@ -27,6 +30,16 @@ object Dependencies {
 
     const val ACTIVITY_EXT = "androidx.activity:activity-ktx:1.3.1"
     const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:1.3.6"
+
+    const val DAGGER2 = "com.google.dagger:dagger:${Version.DAGGER2_VER}"
+    const val DAGGER2_ANDROID = "com.google.dagger:dagger-android:${Version.DAGGER2_VER}"
+    const val DAGGER2_ANDROID_SUPPORT = "com.google.dagger:dagger-android-support:${Version.DAGGER2_VER}"
+
+    const val DAGGER2_COMPILER_KAPT = "com.google.dagger:dagger-compiler:${Version.DAGGER2_VER}"
+    const val DAGGER2_ANDROID_PROCESSOR_KAPT = "com.google.dagger:dagger-android-processor:${Version.DAGGER2_VER}"
+
+    const val DAGGER2_ASSISTED_INJECT_ANNOTATION = "com.squareup.inject:assisted-inject-annotations-dagger2:${Version.ASSISTED_INJECT_VER}"
+    const val DAGGER2_ASSISTED_INJECT_PROCESSOR_KAPT = "com.squareup.inject:assisted-inject-processor-dagger2:${Version.ASSISTED_INJECT_VER}"
 }
 
 object TestConfig {
